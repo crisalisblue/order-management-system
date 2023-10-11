f01f86ade147b6421bf15ec6b31387d0002606f8
lucascn21 <lucascn21@gmail.com>
2023-10-11 13:14:46 -0300

    reestructurado de paginas

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,80 +1,76 @@
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import './Login.css';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import "./Login.css";
 
 const Copyright = (props) => {
   return (
-    <Box className='copyright'>
-      <Typography  variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyright © '}
-          Finnegans Equipo Blue
-          {' '}
-        {new Date().getFullYear()}
+    <Box className="copyright">
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        Finnegans Equipo Blue {new Date().getFullYear()}
       </Typography>
     </Box>
   );
-}
+};
 
-const Login = () => {
+export const Login = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
   return (
-      <Container component="main" maxWidth="xs">
-        <Box className='login-box'>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Iniciar Sesion
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="contraseña"
-              label="Contraseña"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Box className='btn-login'>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-              >
-                Ingresar
-              </Button>
-            </Box>
+    <Container component="main" maxWidth="xs">
+      <Box className="login-box">
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Iniciar Sesion
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="contraseña"
+            label="Contraseña"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Box className="btn-login">
+            <Button type="submit" fullWidth variant="contained">
+              Ingresar
+            </Button>
           </Box>
         </Box>
-        <Copyright/>
-      </Container>
+      </Box>
+      <Copyright />
+    </Container>
   );
-}
-
-
-export default Login;
+};
