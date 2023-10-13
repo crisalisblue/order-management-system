236bdc7a1dbfb319bf7116b6e84a533a7b6924d4
Tomás Acosta <tomas23058@gmail.com>
2023-10-13 17:19:04 -0300

    refactor routes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,24 +4,19 @@ import { Home } from "./pages/Home.jsx";
 // import  UserCRUD  from "./pages/UserCRUD.jsx";
 import Login from "./pages/Login";
 import Layout from "./pages/Layout/Layout";
-import NotLogedinRoute from "./routes/NotLogedinRoute";
+import NotLogedinRoute from './routes/NotLogedinRoute'
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" Component={Layout}>
-          <Route path="/home" Component={Home} />
-          {/* <Route path="/userCRUD" Component={UserCRUD}></Route> */}
+          <Route path="/home" Component={Home}/>
+        {/* <Route path="/userCRUD" Component={UserCRUD}></Route> */}
         </Route>
-        <Route
-          path="/login"
-          element={
-            <NotLogedinRoute>
-              <Login />
-            </NotLogedinRoute>
-          }
-        />
+        <Route Component={NotLogedinRoute}>
+          <Route path="/login" Component={Login}/>
+        </Route>
       </Routes>
     </Router>
   );
