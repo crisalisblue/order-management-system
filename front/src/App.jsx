d8b9ad60345dd457d2fd346fd40f40f9bce1817e
lucascn21 <lucascn21@gmail.com>
2023-10-16 23:07:08 -0300

    Revert "Merge pull request #11 from crisalisblue/SCRUM-14"
    This reverts commit ffa09fdfaa38b6c9e3d8652dc02216ad0915e514, reversing
    changes made to 9081a5480657b369f1874e7da262ced1db3bf544.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,27 +1,11 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import "./App.css";
-import { Home } from "./pages/Home/Home.jsx";
-import { UserCRUD } from "./pages/UserCRUD/UserCRUD.jsx";
-import { Login } from "./pages";
-import { Layout } from "./pages/Layout/Layout";
-import { UserCreate } from "./pages/UserCRUD/UserCreate";
-import { UsersTable } from "./pages/UserCRUD/UsersTable";
-import { UserUpdate } from "./pages/UserCRUD/UserUpdate";
+import './App.css'
 
-export default function App() {
+function App() {
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" Component={Layout}>
-          <Route path="home" Component={Home} />
-          <Route path="userCRUD" Component={UserCRUD}>
-            <Route path="nuevo" Component={UserCreate} />
-            <Route path=":id/editar" Component={UserUpdate} />
-            <Route path="" Component={UsersTable} />
-          </Route>
-        </Route>
-        <Route path="/login" Component={Login}></Route>
-      </Routes>
-    </Router>
-  );
+    <>
+    </>
+  )
 }
+
+export default App
--- front/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { Home } from "./Home/Home";
-export { Layout } from "./Layout/Layout";
-export { Login } from "./Login/Login";
-export { UserCRUD } from "./UserCRUD/UserCRUD";
