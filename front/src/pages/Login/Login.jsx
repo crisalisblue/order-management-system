9c6dbfe61f7191e162b11738ac427a3f08c896ac
Tomás Acosta <tomas23058@gmail.com>
2023-10-18 11:12:41 -0300

    login & errores

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -9,29 +9,37 @@ import "./Login.css";
 import { useAuthContext } from "../../context/UseAuthContext";
 import Copyright from "../../components/Copyrigth";
 import { useState } from "react";
+import { useNavigate } from "react-router";
+import { Alert } from "@mui/material";
 
 export const Login = () => {
-
-  const {loginUser} = useAuthContext();
+  const { loginUser } = useAuthContext();
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
   const [user, setUser] = useState({
     username: "",
-    password: ""
+    password: "",
   });
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      username: data.get("username"),
-      password: data.get("password"),
-    });
-    // setUser(data.get("email"),)
-    loginUser()
+    console.log(user);
+    loginUser(user)
+      .then(() => {
+        setError(null);
+        navigate("/");
+      })
+      .catch(() => setError("Usuario o contraseña incorrectos"));
   };
 
   return (
     <Container component="main" maxWidth="xs">
       <Box className="login-box">
+        {error && (
+          <Alert severity="error" sx={{ my: 2 }}>
+            {error}
+          </Alert>
+        )}
         <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
           <LockOutlinedIcon />
         </Avatar>
@@ -47,17 +55,20 @@ export const Login = () => {
             label="Nombre de usuario"
             name="username"
             autoComplete="username"
-            autoFocus
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
           />
           <TextField
             margin="normal"
             required
             fullWidth
-            name="contraseña"
+            name="password"
             label="Contraseña"
             type="password"
             id="password"
             autoComplete="current-password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
           <Box className="btn-login">
             <Button type="submit" fullWidth variant="contained">
--- front/src/services/UserService.js
+++ front/src/services/UserService.js
@@ -3,5 +3,5 @@ import axios from "axios"
 export const login = (dataBody) => {
     return axios.post("http://localhost:8080/user/login", dataBody)
         .then((response) => ({"user": response.data, "token": response.headers['auth']}))
-        .catch((err) => Promise.reject(err.response.data))
+        .catch((err) => Promise.reject(err))
 }
