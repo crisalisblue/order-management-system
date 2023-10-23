4c8c79d9ad8a9be40f0ddc19838828fce078b205
lucascn21 <lucascn21@gmail.com>
2023-10-23 03:49:57 -0300

    agrego toast

--- front/src/pages/Layout/Layout.jsx
+++ front/src/pages/Layout/Layout.jsx
@@ -1,15 +1,37 @@
 import { Outlet } from "react-router";
 import "./Layout.css";
-import {Navbar} from "../../components/Navbar/Navbar.jsx";
+import { Navbar } from "../../components/Navbar/Navbar.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Layout = () => {
   return (
     <>
       <nav>
-          <Navbar/>
+        <Navbar />
       </nav>
       <main>
         <Outlet />
+        <ToastContainer
+          position="top-right" // Adjust the position
+          autoClose={5000} // Automatically close after 5 seconds
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          // Override container style
+          style={{
+            background: "transparent",
+          }}
+          // Override toast style
+          toastStyle={{
+            background: "transparent",
+            color: "#333",
+          }}
+        />
       </main>
       <footer>footer</footer>
     </>
