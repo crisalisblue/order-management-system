72bcfb6ad97e52b96479d383adb4b02728617378
Sebastian Butocvich <98119390+sebastian-butcovich@users.noreply.github.com>
2023-10-23 10:06:12 -0300

    Limpiado del espacio de trabajo

--- back/src/main/java/crisalis/blue/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package crisalis.blue.repositories;
-
-import crisalis.blue.models.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
