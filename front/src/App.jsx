badaf78453f498923ea3bffd7fe7dc75f540325c
lucascn21 <lucascn21@gmail.com>
2023-10-23 03:50:58 -0300

    reestructurado

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home/Home.jsx";
-import { UserCRUD } from "./pages/UserCRUD/UserCRUD.jsx";
+import { UserCRUD } from "./pages/Usuarios/UserCRUD.jsx";
 import { Login } from "./pages";
 import { Layout } from "./pages/Layout/Layout";
-import { UserCreate } from "./pages/UserCRUD/UserCreate";
-import { UsersTable } from "./pages/UserCRUD/UsersTable";
-import { UserUpdate } from "./pages/UserCRUD/UserUpdate";
+import { UserCreate } from "./pages/Usuarios/UserCreate";
+import { UsersTable } from "./pages/Usuarios/UsersTable";
+import { UserUpdate } from "./pages/Usuarios/UserUpdate";
 import NotLogedinRoute from "./routes/NotLogedinRoute";
 import LogedinRoute from "./routes/LogedinRoute";
 // import NotLogedinRoute from "./routes/NotLogedinRoute";
@@ -18,7 +18,7 @@ export default function App() {
         <Route Component={LogedinRoute}>
           <Route path="/" Component={Layout}>
             <Route path="home" Component={Home} />
-            <Route path="userCRUD" Component={UserCRUD}>
+            <Route path="usuarios" Component={UserCRUD}>
               <Route path="nuevo" Component={UserCreate} />
               <Route path=":id/editar" Component={UserUpdate} />
               <Route path="" Component={UsersTable} />
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -8,13 +8,12 @@ import React from "react";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <CssBaseline />
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <AuthProvider>
+      <CssBaseline />
+      <App />
+    </AuthProvider>
+  </ThemeProvider>
 );
