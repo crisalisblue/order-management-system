7027e3e44912e0c7ecf1f726e89c35b3918fe33a
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-10-25 10:32:43 -0300

    Creado DTO Customer

---
+++ back/src/main/java/crisalis/blue/models/dto/CustomerDTO.java
@@ -0,0 +1,45 @@
+package crisalis.blue.models.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerDTO {
+
+    //Atributos de Cliente
+    @JsonProperty("id")
+    private Integer id;
+
+
+    //Atributos de Persona
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @JsonProperty("dni")
+    private String dni;
+
+
+    //Atributos de Empresa
+
+    //cuit
+    @JsonProperty("businessName")
+    private String businessName;
+
+    @JsonProperty("activityStartDate")
+    private Date activityStartDate;
+
+    @JsonProperty("cuit")
+    private Double cuit;
+
+}
