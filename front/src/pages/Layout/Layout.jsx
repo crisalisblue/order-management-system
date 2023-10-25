69edc0b4eb963a995e7378d1bf57773f52011b94
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2023-10-25 09:16:00 -0300

    linting

--- front/src/pages/Layout/Layout.jsx
+++ front/src/pages/Layout/Layout.jsx
@@ -5,16 +5,17 @@ import { NavBar } from "../../components/NavBar/NavBar.jsx";
 export const Layout = () => {
   return (
     <>
-        <header className={"flex items-center justify-center"}>
-            <h1 className={"text-center"}>Finnegans</h1>
-        </header>
-        <nav>
-            <NavBar />
-        </nav>
-        <main>
-            <Outlet />
-        </main>
-        <footer className={"text-center"}>Finnegans Equipo Blue {new Date().getFullYear()}</footer>
+      <header className={"flex items-center justify-center bg-slate-600"}>
+        <h1 className={"text-center"}>Finnegans</h1>
+      </header>
+
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+      <footer className={"text-center"}>
+        Finnegans Equipo Blue {new Date().getFullYear()}
+      </footer>
     </>
   );
 };
