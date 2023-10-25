b16426ec3249c369b0444f4d0cb1842fc37c48c3
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2023-10-25 09:06:52 -0300

    tailwind color theme

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mytheme: {
+          primary: "#111827",
+          secondary: "#ea580c",
+          accent: "#10b981",
+          neutral: "#d1d5db",
+          "base-100": "#1d232a",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
