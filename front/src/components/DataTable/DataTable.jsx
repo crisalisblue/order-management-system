f1a61213728cbb0119a71614092761654f1cd685
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2023-10-25 15:42:57 -0300

    tabla zebra

--- front/src/components/DataTable/DataTable.jsx
+++ front/src/components/DataTable/DataTable.jsx
@@ -1,10 +1,9 @@
 import { DeleteModal } from "../DeleteModal";
 export const DataTable = ({ data }) => {
   return (
-    <table className="table-auto">
+    <table className="table table-zebra table-xs">
       <thead>
         <tr>
-          <th>ID</th>
           <th>Username</th>
           <th>Name</th>
           <th>Password</th>
@@ -13,7 +12,6 @@ export const DataTable = ({ data }) => {
       <tbody>
         {data.map((item, index) => (
           <tr key={index}>
-            <td>{item.id}</td>
             <td>{item.username}</td>
             <td>{item.name}</td>
             <td>{item.password}</td>
@@ -21,7 +19,7 @@ export const DataTable = ({ data }) => {
               <a href={`/usuarios/${item.id}/editar`}>
                 <button className="btn">Editar</button>
               </a>
-              <DeleteModal></DeleteModal>
+              <DeleteModal itemId={item.id}></DeleteModal>
             </td>
           </tr>
         ))}
