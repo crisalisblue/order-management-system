c4dffc9ff9172975e9bfcc6d123df568d7c88ed4
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2023-10-25 10:58:54 -0300

    arreglo formulario cracion

--- front/src/pages/Usuarios/UserCreate.jsx
+++ front/src/pages/Usuarios/UserCreate.jsx
@@ -2,18 +2,22 @@ import { createSingleUser } from "../../api/UserAPI";
 import { useForm } from "react-hook-form";
 
 export const UserCreate = () => {
-  const { handleSubmit, control } = useForm();
+  const { register, handleSubmit, reset, control, setValue } = useForm();
   const onSubmit = async (data, e) => {
+    console.dir(data);
     console.dir(await createSingleUser(data));
   };
   const onError = (errors, e) => console.log(errors, e);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit, onError)}>
+    <form
+      className={"flex justify-evenly"}
+      onSubmit={handleSubmit(onSubmit, onError)}
+    >
       userCreate
-      <input type="text" />
-      <input type="text" />
-      <input type="password" name="" id="" />
+      <input type="text" {...register("name")} />
+      <input type="text" {...register("username")} />
+      <input type="password" {...register("password")} />
       <input type="submit" />
     </form>
   );
