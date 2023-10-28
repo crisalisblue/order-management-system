bd7830e898bc7dff49aabb626de12b1f0b5ebda0
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2023-10-28 17:51:37 -0300

    main auto id por ruta

--- front/src/pages/Layout/Layout.jsx
+++ front/src/pages/Layout/Layout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router";
 import "./Layout.css";
 import { NavBar } from "../../components/NavBar/NavBar.jsx";
+import { useLocation } from "react-router-dom";
 
 export const Layout = () => {
+  const location = useLocation();
+  const mainId = location.pathname.substring(1);
+
   return (
     <>
       <NavBar />
-      <main>
+      <main id={mainId}>
         <Outlet />
       </main>
       <footer className={"text-center"}>
