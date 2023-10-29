24d6b79172a516826d25664a8020a3e06ed0b529
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2023-10-29 20:55:00 -0300

    agrego filtros mock

--- front/src/pages/Usuarios/Usuarios.jsx
+++ front/src/pages/Usuarios/Usuarios.jsx
@@ -3,8 +3,46 @@ import { Outlet } from "react-router-dom";
 export const Usuarios = () => {
   return (
     <>
+      <article className="bg-gray-500 prose min-w-full">
+        <h1 className="text-primary text-center m-0 p-0">Usuarios</h1>
+        <section id="filters" className="flex items-center gap-4">
+          <label htmlFor="filter1" className="mr-4">
+            Filtrar Por:
+          </label>
+          <select
+            id="filter1"
+            className="select select-xs select-bordered max-w-xs"
+          >
+            <option disabled selected>
+              Filtros 1
+            </option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+          </select>
+          <select className="select select-xs select-bordered max-w-xs">
+            <option disabled selected>
+              Filtros 2
+            </option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+          </select>
+          <select className="select select-xs select-bordered max-w-xs">
+            <option disabled selected>
+              Filtros 3
+            </option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+          </select>
+          <select className="select select-xs select-bordered max-w-xs">
+            <option disabled selected>
+              Filtros 4
+            </option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+          </select>
+        </section>
+      </article>
       <section className="prose overflow-auto max-h-[80dvh] min-w-full">
-        <h1 className="text-primary text-center  m-0 p-0">Usuarios</h1>
         <Outlet />
       </section>
     </>
