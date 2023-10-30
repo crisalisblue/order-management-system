ebe8ef0431f3b3a0fe717651381e6ab8d4b08ee4
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-10-30 11:50:31 -0300

    Creado model Tax

--- back/src/main/java/crisalis/blue/models/Tax.java
+++ back/src/main/java/crisalis/blue/models/Tax.java
@@ -1,4 +1,46 @@
 package crisalis.blue.models;
 
+import crisalis.blue.models.dto.TaxDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@Entity
+@Table(name = "tax")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tax {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(
+            name = "name",
+            nullable = false)
+    private String name;
+
+    @Column(name = "percentage")
+    private BigInteger percentage;
+
+    //Montofijo
+    @Column(name = "fixedAmount")
+    private BigInteger fixedAmount;
+
+    public Tax ToDTO(){
+        return
+                TaxDTO
+                        .builder()
+                        .id(this.id)
+                        .name(this.name)
+                        .percentage(this.percentage)
+                        .fixedAmount(this.fixedAmount)
+                        .build();
+    }
+
+    //Falta agregar relacion con otras tablas...
 }
