302857129e2191b5280bc0b937acdfed09ab5ca1
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2023-11-06 11:41:36 -0300

    cambio del fondo

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -11,8 +11,8 @@ export default {
       {
         mytheme: {
           primary: "#001F3D",
-          secondary: "#4C73A0",
-          accent: "#00A4E6",
+          secondary: "#678fc0",
+          accent: "#006591",
           neutral: "#272835",
           "base-100": "#CCE3ED",
           info: "#CCE3ED",
