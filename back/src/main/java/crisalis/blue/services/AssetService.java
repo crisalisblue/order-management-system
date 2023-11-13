c750fadabb81582f9e601cb521d960d7f85aebe2
Sebastian Butocvich <sebabutcovich@gmail.com>
2023-11-13 10:54:09 -0300

    BackEnd de impuestos por  productos/servicio V-3

--- back/src/main/java/crisalis/blue/services/AssetService.java
+++ back/src/main/java/crisalis/blue/services/AssetService.java
@@ -112,7 +112,7 @@ public class AssetService {
             if (assetDTO.getBaseAmount().intValue() != 0)
                 aux.get().setBaseAmount(assetDTO.getBaseAmount());
             if(assetDTO.getTaxDTOList() != null)
-                buscarTax(assetDTO.getTaxDTOList())
+                buscarTax(assetDTO.getTaxDTOList());
             if(assetDTO.getSupportFree() != null && assetDTO.getSupportFree().intValue() != 0) {
                 service = (crisalis.blue.models.Service) aux.get();
                 service.setSupportFree(assetDTO.getSupportFree());
