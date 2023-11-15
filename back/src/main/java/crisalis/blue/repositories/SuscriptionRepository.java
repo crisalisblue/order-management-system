980fd2369387946a4f4c267a53588f547a27ae44
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-11-15 11:21:59 -0300

    Creado repo Suscription

--- back/src/main/java/crisalis/blue/repositories/SuscriptionRepository.java
+++ back/src/main/java/crisalis/blue/repositories/SuscriptionRepository.java
@@ -1,4 +1,8 @@
 package crisalis.blue.repositories;
 
-public interface SuscriptionRepository {
+import crisalis.blue.models.Suscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface SuscriptionRepository extends JpaRepository<Suscription, Long> {
 }
