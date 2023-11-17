5c1dc1aec1c717fe4bab1f856aa7e5d0ee208a55
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-11-16 22:08:09 -0300

    Creado Model de Subscription

--- back/src/main/java/crisalis/blue/models/dto/SubscriptionDTO.java
+++ back/src/main/java/crisalis/blue/models/dto/SubscriptionDTO.java
@@ -1,4 +1,25 @@
 package crisalis.blue.models.dto;
 
-public class SubscriptionDTO { //modificar esto
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SubscriptionDTO {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("customer")
+    private Long customer;
+
+    @JsonProperty("service")
+    private Long service;
+
+    @JsonProperty("status")
+    private Boolean status;
 }
