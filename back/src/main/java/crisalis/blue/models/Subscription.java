9b970645c3cd5710ebd1b72c64b5e499c17f8843
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-11-16 22:08:21 -0300

    Creado Model de Subscription

--- back/src/main/java/crisalis/blue/models/Subscription.java
+++ back/src/main/java/crisalis/blue/models/Subscription.java
@@ -1,20 +1,54 @@
 package crisalis.blue.models;
 
+import crisalis.blue.models.dto.CustomerDTO;
+import crisalis.blue.models.dto.SubscriptionDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Data
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(
-        name ="type",
-        discriminatorType = DiscriminatorType.STRING
-)
-@Table(name = "customer")
+@Table(name = "subscription")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class Subscription {//modificar esto
+public class Subscription {
+
+    public Subscription(SubscriptionDTO dto){
+        setStatus(this.status);
+        setCustomer(this.customer);
+        setService(this.service);
+    }
+
+    @Id
+    @SequenceGenerator(
+            name = "subscription_sequence",
+            sequenceName = "subscription_sequence",
+            allocationSize = 1,
+            initialValue = 1
+
+    )
+    @Column(name = "id")
+    private Long id;
+
+    @Column
+    private Boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_customer")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_service")
+    private Service service;
+
+    public SubscriptionDTO toDTO(){
+        SubscriptionDTO dto = new SubscriptionDTO();
+        dto.setId(this.id);
+        dto.setStatus(this.status);
+        dto.setCustomer(this.customer.getId());
+        dto.setService(this.customer.getId());
+        return dto;
+    }
 }
