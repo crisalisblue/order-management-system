df6ca7a7d03eca01cb94c8373f95c5cc3d32245f
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-11-24 19:28:46 -0300

    Agregados metodos find y exists, by customer y asset

--- back/src/main/java/crisalis/blue/repositories/SubscriptionRepository.java
+++ back/src/main/java/crisalis/blue/repositories/SubscriptionRepository.java
@@ -2,7 +2,12 @@ package crisalis.blue.repositories;
 
 import crisalis.blue.models.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    Boolean existsByCustomerId(@Param("customer_id") Long id);
+    Boolean existsByAssetId(@Param("asset_id") Long id);
+    Subscription findByCustomerId(@Param("customer_id") Long id);
+    Subscription findByAssetId(@Param("asset_id") Long id);
 }
