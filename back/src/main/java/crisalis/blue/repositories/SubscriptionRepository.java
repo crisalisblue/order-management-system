95aaa7c189b50c95ac57829dc319273bdab0bbb1
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-11-26 23:38:26 -0300

    los find ahora devuelven una lista

--- back/src/main/java/crisalis/blue/repositories/SubscriptionRepository.java
+++ back/src/main/java/crisalis/blue/repositories/SubscriptionRepository.java
@@ -4,10 +4,13 @@ import crisalis.blue.models.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
     Boolean existsByCustomerId(@Param("customer_id") Long id);
     Boolean existsByAssetId(@Param("asset_id") Long id);
-    Subscription findByCustomerId(@Param("customer_id") Long id);
-    Subscription findByAssetId(@Param("asset_id") Long id);
+    List<Subscription> findAllByCustomerId(@Param("customer_id") Long id);
+    List<Subscription> findAllByAssetId(@Param("asset_id") Long id);
 }
