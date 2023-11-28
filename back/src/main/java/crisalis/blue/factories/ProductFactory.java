dab6ea0ccdd2b53ce0a4569814a4306b6d3ca2fb
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-11-28 19:48:36 -0300

    Agregado Patron Singleton

--- back/src/main/java/crisalis/blue/factories/ProductFactory.java
+++ back/src/main/java/crisalis/blue/factories/ProductFactory.java
@@ -7,6 +7,16 @@ import java.math.BigDecimal;
 
 public class ProductFactory extends ItemFactory{
 
+    //Aplicando Singleton
+    //instancia unica
+    private static final ProductFactory instancia = new ProductFactory();
+    //Constructor privado para evitar que se instancie externamente
+    private ProductFactory() {}
+    //Metodo para acceder a la instancia unica
+    public static ProductFactory getInstance() {
+        return instancia;
+    }
+
     @Override
     public Asset crearItem(){
         Product product = new Product();
--- back/src/main/java/crisalis/blue/factories/ServiceFactory.java
+++ back/src/main/java/crisalis/blue/factories/ServiceFactory.java
@@ -6,6 +6,18 @@ import crisalis.blue.models.Servicie;
 import java.math.BigDecimal;
 
 public class ServiceFactory extends ItemFactory{
+
+    //Aplicando Singleton
+    //instancia unica
+    private static final ServiceFactory instancia = new ServiceFactory();
+    //Constructor privado para evitar que se instancie externamente
+    private ServiceFactory() {}
+    //Metodo para acceder a la instancia unica
+    public static ServiceFactory getInstance() {
+        return instancia;
+    }
+
+
     public Asset crearItem(){
         Servicie service = new Servicie();
         service.setSupportFee(BigDecimal.valueOf(0));
