eb30eeff76cbcd1971f602556852a3fb9e47bf80
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2023-11-29 02:04:30 -0300

    aplicados muchos arreglos al front

--- front/src/pages/Impuestos/TaxCreate.jsx
+++ front/src/pages/Impuestos/TaxCreate.jsx
@@ -29,28 +29,61 @@ export const TaxCreate = () => {
   };
 
   return (
-    <section id="userForm" className="bg-base-200 prose min-w-full">
+    <section id="taxCreate" className="w-5/6 prose min-w-full">
       <form
-        className="gap-4 p-4 flex flex-col"
+        className="w-5/6 mx-auto p-6 bg-white rounded-md shadow-md"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div>
-          <label htmlFor="name">Nombre:</label>
-          <input type="text" {...register("name")} />
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block text-sm font-semibold text-gray-600"
+          >
+            Nombre:
+          </label>
+          <input
+            type="text"
+            id="name"
+            {...register("name")}
+            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          />
         </div>
 
-        <div>
-          <label htmlFor="username">Porcentaje:</label>
-          <input type="text" {...register("percentage")} />
+        <div className="mb-4">
+          <label
+            htmlFor="percentage"
+            className="block text-sm font-semibold text-gray-600"
+          >
+            Porcentaje:
+          </label>
+          <input
+            type="text"
+            id="percentage"
+            {...register("percentage")}
+            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          />
         </div>
 
-        <div>
-          <label htmlFor="password">Monto fijo:</label>
-          <input type="text" {...register("baseAmount")} />
+        <div className="mb-4">
+          <label
+            htmlFor="baseAmount"
+            className="block text-sm font-semibold text-gray-600"
+          >
+            Monto fijo:
+          </label>
+          <input
+            type="text"
+            id="baseAmount"
+            {...register("baseAmount")}
+            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          />
         </div>
 
-        <button className="btn bg-primary text-base-100" type="submit">
-          Agregar
+        <button
+          type="submit"
+          className="w-1/3 px-4 py-2 text-white bg-primary rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          Agregar Impuesto
         </button>
       </form>
     </section>
